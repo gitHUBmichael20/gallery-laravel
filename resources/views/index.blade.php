498ef52c6ecc00a1159d953ceb7bef733ae77b34
Michael Simbolon <carlosimbolon23@gmail.com>
2025-01-21 21:07:57 +0700

    small changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,7 +30,7 @@
                             d="M1 5h12m0 0L9 1m4 4L9 9" />
                     </svg>
                 </a>
-                <a href="https://github.com/gitHUBmichael20"
+                <a href="https://github.com/gitHUBmichael20" target="_blank"
                     class="inline-flex gap-2 justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
                     Connect <i class="fa-brands fa-github fa-xl" style="color: #B197FC;"></i>
                 </a>
